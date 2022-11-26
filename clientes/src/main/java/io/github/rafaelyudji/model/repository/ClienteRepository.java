8f26698df827f57d0d6e66c7e775813b7e985dee
Yudji <84600861+rafaelyudji@users.noreply.github.com>
2022-11-25 21:40:29 -0300

    Delete clientes directory

--- clientes/src/main/java/io/github/rafaelyudji/model/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.rafaelyudji.model.repository;
-
-import io.github.rafaelyudji.model.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-    
-
-}
